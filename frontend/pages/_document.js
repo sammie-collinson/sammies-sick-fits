a4f6371d49aef3c68544b4461880c38e74e87851
Sammie Collinson <110931507+sammie-harrys@users.noreply.github.com>
2023-12-16 14:50:05 -0500

    fix: styled components

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,28 +1,54 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import Document, { Html, Head, NextScript, Main } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, {
+    DocumentContext,
+    Html,
+    Head,
+    Main,
+    NextScript,
+  } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-    // waits until render method is resolved before sending stylesheet data to the browser (avoids 'className not matching' errors)
-    // https://styled-components.com/docs/advanced#server-side-rendering
-    static getInitialProps({ renderPage }) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage(
-            (App) => (props) => sheet.collectStyles(<App {...props} />)
-        );
-        const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
+  
+  export default class MyDocument extends Document {
+    /**
+     * https://styled-components.com/docs/advanced#nextjs
+     */
+    static async getInitialProps(ctx) {
+      const sheet = new ServerStyleSheet()
+      const originalRenderPage = ctx.renderPage
+  
+      try {
+        ctx.renderPage = () =>
+          originalRenderPage({
+            enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          })
+        const initialProps = await Document.getInitialProps(ctx)
+        return {
+          ...initialProps,
+          styles: (
+            <>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+            </>
+          ),
+        }
+      } finally {
+        sheet.seal()
+      }
     }
-
+  
+    /**
+     * https://xstyled.dev/docs/dark-mode/#next
+     */
     render() {
-        return (
-            <Html lang="en-US">
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+      return (
+        <Html>
+          <Head />
+          <body>
+           
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      )
     }
-}
+  }
